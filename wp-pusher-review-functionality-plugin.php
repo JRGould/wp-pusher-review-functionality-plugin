4046c0c510d2e8614037327a3b6b38516bc54c1d
Jeff Gould <jrgould@gmail.com>
2016-01-19 12:09:11 -0800

    add plugin boilerplate

---
+++ wp-pusher-review-functionality-plugin.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: WP Pusher Review Site Functionality
+Plugin URI: http://deliciousbrains.com/blog
+Description: Functionality plugin for WP Pusher Review Site
+Author: Jeff Gould
+Version: 0.1
+Author URI: http://deliciousbrains.com
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+
+class WP_Pusher_Review {
+	function __construct() {
+		// Register hooks here
+	}
+}
+new WP_Pusher_Review()
+
