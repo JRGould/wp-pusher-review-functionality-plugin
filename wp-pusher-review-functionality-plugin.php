748580de7cbdfbe0df039e13251e0082d0e69ffa
Jeff Gould <jrgould@gmail.com>
2016-01-21 08:32:23 -0800

    added busey-me shortcode

--- wp-pusher-review-functionality-plugin.php
+++ wp-pusher-review-functionality-plugin.php
@@ -18,10 +18,23 @@ class WP_Pusher_Review {
 		// Register hooks here
 		error_log( 'Func test plugin constructed...' );
 		register_activation_hook( __FILE__, [ $this, 'plugin_was_activated' ] );
+		add_shortcode( 'busey-me', [ $this, 'busey_me' ] );
 	}
 
 	public function plugin_was_activated() {
 		//activation hook
+	}
+
+	public function busey_me() {
+		$response = wp_remote_get( 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=Gary+Busey' );
+		if ( is_wp_error( $response ) ){
+			return;
+		}
+
+		$response = json_decode( $response['body'], true );
+		$gif = $response['data']['image_url'];
+
+		return "<img src='{$gif}' alt='Utah, gimme two!' />";
 	}
 }
 new WP_Pusher_Review();
